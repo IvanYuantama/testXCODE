7ad84ad359075d226832a765919cca6e1b124861
Ivan Yuantama <ivanyp59@gmail.com>
2026-05-20 13:23:48 +0800

    Initial Commit

---
+++ testGITHUB/testGITHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGITHUBApp.swift
+//  testGITHUB
+//
+//  Created by Ivan Yuantama Pradipta on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct testGITHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
